74f78bba990cb41d5136d81224fd4084f24c6844
Anugeeth <anugeeth2@gmail.com>
2020-07-06 03:38:11 +0530

    added adminfinder

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const readline = require('readline');
+const figlet = require('figlet');
 
 const common = require('./src/common');
 const admin = require('./src/admin');
 
 (function () {
+    let url = '';
+    var option = 0;
 
-    const rl = readline.createInterface({
+    displayBanner();
+
+     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-
-    let url = '';
-    var option = 0;
-    console.log("1  - Whois Lookup")
-    console.log("2  - NameServer Lookup")
-    console.log("3  - Find IP address location")
-    console.log("4  - TCP Port Scan")
-    console.log("5  - Reverse IP Lookup")
-    console.log("6  - Forward DNS search")
-    console.log("7  - HTTP Headers Check")
-    console.log("8  - Zone Transfer")
-    console.log("9  - Admin Panel Search")
-
-
-
 
     rl.question(`Select an option?\t`, (value) => {
         option = Number(value);
@@ -37,6 +27,24 @@ const admin = require('./src/admin');
     return;
 }());
 
+
+function displayBanner() {
+ figlet('recon.js', (err, data) => {
+            if (!err) {
+                console.log(data)
+                console.log("1  - Whois Lookup")
+                console.log("2  - NameServer Lookup")
+                console.log("3  - Find IP address location")
+                console.log("4  - TCP Port Scan")
+                console.log("5  - Reverse IP Lookup")
+                console.log("6  - Forward DNS search")
+                console.log("7  - HTTP Headers Check")
+                console.log("8  - Zone Transfer")
+                console.log("9  - Admin Panel Search")
+
+            }
+        });
+}
 
 function choices(option, url) {
     switch (option) {
